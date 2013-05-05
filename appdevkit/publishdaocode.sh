8341ffa3300862935fc184c6b0928ac1f6c6a1a5
kyledinh <kyledinh@gmail.com>
2013-05-04 21:26:33 -0700

    publishdaocode.sh now read conf for appname property

--- appdevkit/publishdaocode.sh
+++ appdevkit/publishdaocode.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cp build/*.scala ../src/main/scala/taakapp/
+
+CONF=daogenerator.conf
+
+if [ -f $CONF ];
+then 
+	echo "Using the $CONF "
+else
+	echo "Configuration file $CONF not found"
+	exit 1
+fi	
+
+APP_NAME=`sed '/^\#/d' ../conf/$CONF | grep 'app.name'  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+
+cp build/*.scala ../src/main/scala/$APP_NAME/
 
